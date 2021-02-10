34203fbe4d057de3d871c120fc8adab8fbe5fcf4
Roxana Meixner <roxana.meixner@ubirch.com>
2021-02-10 13:09:05 +0100

    comments for UPP related constants

--- ubirch/ubirch_protocol.go
+++ ubirch/ubirch_protocol.go
@@ -30,10 +30,10 @@ import (
 type ProtocolVersion uint8
 
 const (
-	Signed           ProtocolVersion = 0x22 // Signed protocol, the payload is signed
-	Chained          ProtocolVersion = 0x23 // Chained protocol, the payload contains the previous signature and is signed
-	minSignedUPPLen                  = 90
-	minChainedUPPLen                 = 156
+	Signed           ProtocolVersion = 0x22 // Signed protocol, the Ubirch Protocol Package is signed
+	minSignedUPPLen                  = 90   // minimal length of a signed Ubirch Protocol Package
+	Chained          ProtocolVersion = 0x23 // Chained protocol, the Ubirch Protocol Package contains the previous signature and is signed
+	minChainedUPPLen                 = 156  // minimal length of a chained Ubirch Protocol Package
 )
 
 // Crypto Interaface for exported functionality
