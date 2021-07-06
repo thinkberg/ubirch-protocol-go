fb65facf058b8a61467eed5a62402b44ef5ca45d
ub-docz <ub-docz@users.noreply.github.com>
2021-07-06 17:23:05 +0200

    Minor: Update compatibility comment

--- ubirch/crypto_test.go
+++ ubirch/crypto_test.go
@@ -156,7 +156,7 @@ func TestCryptoContext_SetKey(t *testing.T) {
 	//create golang or pkcs#11 crypto context depending on test settings
 	context, err := getCryptoContext()
 	requirer.NoError(err, "creating crypto context failed")
-	defer context.Close()
+	defer context.Close() //TODO: how to handle error?
 
 	id := uuid.MustParse(defaultUUID)
 	privBytesCorrect, err := hex.DecodeString(defaultPriv)
@@ -194,12 +194,13 @@ func TestCryptoContext_SetKey(t *testing.T) {
 	}
 }
 
-// TestCryptoContext_SetPublicKey Tests the set function for a public key. Incompatible with pkcs#11 crypto. (No SetPublicKey() for HSMs)
+// TestCryptoContext_SetPublicKey Tests the set function for a public key.
 //		Set a public key with correct length
 //		Set a public key, which is too long
 //		Set a public key, which is too short
 //		Set a public key, which is nil
 //		Set a public key, which has correct length but is an invalid elliptic curve public key value
+//TODO: add support for pkcs#11 crypto
 func TestCryptoContext_SetPublicKey(t *testing.T) {
 	asserter := assert.New(t)
 	requirer := require.New(t)
@@ -240,7 +241,7 @@ func TestCryptoContext_GenerateKey(t *testing.T) {
 	//create golang or pkcs#11 crypto context depending on test settings
 	context, err := getCryptoContext()
 	requirer.NoError(err, "creating crypto context failed")
-	defer context.Close()
+	defer context.Close() //TODO: how to handle error?
 
 	p := NewExtendedProtocol(context, map[uuid.UUID][]byte{})
 
